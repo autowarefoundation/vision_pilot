447a5385621c252d141f165118952f8c55e34f64
TranHuuNhatHuy <huy9515@gmail.com>
2026-05-04 14:53:37 +0900

    init first ROS2ImageSubscriber functionaliy boilerplate

--- VisionPilot/development_releases/1.0/modules/middleware_interfaces/ros2_interface/camera_subscriber/src/ros2_to_opencv.cpp
+++ VisionPilot/development_releases/1.0/modules/middleware_interfaces/ros2_interface/camera_subscriber/src/ros2_to_opencv.cpp
@@ -1,1 +1,20 @@
 #include <camera_subscriber/ros2_to_opencv.hpp>
+
+namespace camera_subscriber {
+
+    ROS2ImageSubscriber::ROS2ImageSubscriber(
+        const std::string &topic_name,
+        size_t queue_size,
+        const std::string &node_name
+    ) : (
+        rclcpp::Node(node_name),
+        max_queue_size_(queue_size)
+    ) {
+        
+        RCLCPP_INFO(get_logger(), "Initializing ROS2 Image Subscriber");
+        RCLCPP_INFO(get_logger(), "  Topic: %s", topic_name.c_str());
+        RCLCPP_INFO(get_logger(), "  Queue Size: %zu", queue_size);
+
+    };
+
+}
