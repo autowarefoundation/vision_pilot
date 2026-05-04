297b0d92d1b2ac8e43d3c8181c22586b80575a6e
TranHuuNhatHuy <huy9515@gmail.com>
2026-05-04 15:03:34 +0900

    implement ROS2 subscription to image topic

--- VisionPilot/development_releases/1.0/modules/middleware_interfaces/ros2_interface/camera_subscriber/src/ros2_to_opencv.cpp
+++ VisionPilot/development_releases/1.0/modules/middleware_interfaces/ros2_interface/camera_subscriber/src/ros2_to_opencv.cpp
@@ -15,6 +15,28 @@ namespace camera_subscriber {
         RCLCPP_INFO(get_logger(), "  Topic: %s", topic_name.c_str());
         RCLCPP_INFO(get_logger(), "  Queue Size: %zu", queue_size);
 
+        stats_.node_name = node_name;
+
+        // Create subscription to the image topic
+        // Using rclcpp::QoS profile for best effort delivery (suitable for camera streams)
+        // Reference:
+        //      - https://docs.ros.org/en/iron/Concepts/Intermediate/About-Quality-of-Service-Settings.html
+        //      - https://docs.ros2.org/foxy/api/rclcpp/classrclcpp_1_1QoS.html
+        
+        auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(queue_size))
+            .best_effort()
+            .durability_volatile();
+
+        image_subscription_ = create_subscription<sensor_msgs::msg::Image>(
+            topic_name,
+            qos_profile,
+            [this](const sensor_msgs::msg::Image::SharedPtr msg) {
+                this->image_callback(msg);
+            }
+        );
+
+        RCLCPP_INFO(get_logger(), "ROS2 Image Subscriber initialized successfully");
+
     };
 
 }
