ed5089207c888542714d923e085df104dbee652e
TranHuuNhatHuy <huy9515@gmail.com>
2026-05-11 10:09:11 +0900

    header : add constructor and destructor for V4L2

--- VisionPilot/development_releases/1.0/modules/sensing/v4l2_interface/include/v4l2_interface/v4l2_reader.hpp
+++ VisionPilot/development_releases/1.0/modules/sensing/v4l2_interface/include/v4l2_interface/v4l2_reader.hpp
@@ -9,4 +9,35 @@
 #include <memory>
 #include <iostream>
 
+namespace v4l2_interface {
+
+    class V4L2Reader {
+
+        public:
+
+        /**
+        * @brief Constructor for V4L2Reader
+        *
+        * @param device_path Mounting path to V4L2 device (e.g., "/dev/video0")
+        @ @param fps Desired FPS for outputing frames. Current default is 10.
+        * 
+        * Inits the V4L2 camera device with specified parameters and start capture/vis thread.
+        * Also logs these details and subscribtion info.
+        */    
+        explicit V4L2Reader(
+            const std::string& device_path,
+            uint32_t fps = 10
+        );
+
+        /**
+        * @brief Destructor for V4L2Reader
+        *
+        * Properly cleans up camera resources and closes video capture threads.
+        */
+        ~V4L2Reader();
+
+    }
+
+}
+
 #endif //VISIONPILOT_V4L2_READER_HPP
