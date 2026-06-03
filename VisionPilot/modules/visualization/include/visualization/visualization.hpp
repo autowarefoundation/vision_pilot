327f796d4225f035ce778a68085999087ad3297a
TranHuuNhatHuy <huy9515@gmail.com>
2026-06-03 21:46:11 +0900

    defined dims of major compartments in vis

--- VisionPilot/modules/visualization/include/visualization/visualization.hpp
+++ VisionPilot/modules/visualization/include/visualization/visualization.hpp
@@ -3,10 +3,16 @@
 
 #include <opencv2/opencv.hpp>
 
+#include <optional>
+
 #include <string>
 #include <vector>
 
 namespace visualization {
+
+inline constexpr int kFrameWidth = 1024;
+inline constexpr int kFrameHeight = 512;
+inline constexpr int kVisualizationPanelWidth = 360;
 
 bool render_frame(
 	const cv::Mat &frame,
