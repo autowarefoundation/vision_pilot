88ed982af488591e6ce2b55531083fa2b1bcc7f6
TranHuuNhatHuy <huy9515@gmail.com>
2026-06-04 13:53:47 +0900

    test: added bbox reader from test assets

--- VisionPilot/development_releases/1.0/tests/visualization/test_visualization.cpp
+++ VisionPilot/development_releases/1.0/tests/visualization/test_visualization.cpp
@@ -30,4 +30,30 @@ namespace {
 
     };
 
+
+    // Utility func to load bounding boxes from a FileNode, returning a vector of YoloBoundingBox structs
+    std::vector<visualization::YoloBoundingBox> load_bounding_boxes(const cv::FileNode &node) {
+        std::vector<visualization::YoloBoundingBox> bounding_boxes;
+        if (node.empty() || !node.isSeq()) {
+            return bounding_boxes;
+        }
+
+        for (auto it = node.begin(); it != node.end(); ++it) {
+            const std::vector<float> values = read_scalar_sequence<float>(*it);
+            if (values.size() < 5) {
+                continue;
+            }
+
+            visualization::YoloBoundingBox box;
+            box.class_id = static_cast<int>(values[0]);
+            box.center_x = values[1];
+            box.center_y = values[2];
+            box.width = values[3];
+            box.height = values[4];
+            bounding_boxes.push_back(box);
+        }
+
+        return bounding_boxes;
+    }
+
 } // namespace
