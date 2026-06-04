2d634e6518f86e27186ba79c395cb36d43d03b21
TranHuuNhatHuy <huy9515@gmail.com>
2026-06-04 13:53:08 +0900

    test: added scalar reader

--- VisionPilot/development_releases/1.0/tests/visualization/test_visualization.cpp
+++ VisionPilot/development_releases/1.0/tests/visualization/test_visualization.cpp
@@ -11,6 +11,23 @@
 
 namespace {
 
+    // Utility func to read a sequence of scalar values from a FileNode
+    template <typename T>
+    std::vector<T> read_scalar_sequence(const cv::FileNode &node) {
 
+        std::vector<T> values;
+        if (node.empty() || !node.isSeq()) {
+            return values;
+        }
+
+        for (auto it = node.begin(); it != node.end(); ++it) {
+            T value{};
+            *it >> value;
+            values.push_back(value);
+        }
+
+        return values;
+
+    };
 
 } // namespace
